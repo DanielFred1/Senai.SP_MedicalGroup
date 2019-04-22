cac4258e7c5d2e84a24eab6bb86d6b5922c63c2f
Daniel Frederic Sena Da Silva <45867182851@info.local>
2019-04-22 11:18:09 -0300

    Interface e Repositorio de Consulta

---
+++ Senai.SPMedicalGroup/Senai.SPMedicalGroup/Interfaces/IConsultaRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Senai.SPMedicalGroup.Domains;
+
+namespace Senai.SPMedicalGroup.Interfaces
+{
+    interface IConsultaRepository
+    {
+        void Agendar(Consultas consulta);
+
+        void Atualizar(Consultas consulta);
+
+        List<Consultas> Listar();
+
+        /// <summary>
+        /// Busca um Médico pelo Id
+        /// </summary>
+        /// <param name="idmedico">Id</param>
+        /// <returns>Retorna um Medico</returns>
+        List<Consultas> BuscarPorIdMedico(int idmedico);
+
+        /// <summary>
+        /// Busca um Prontuario pelo Id
+        /// </summary>
+        /// <param name="idprontuario">Id</param>
+        /// <returns>Retorna um Prontuario(paciente)</returns>
+        List<Consultas> BuscarPorIdProntuario(int idprontuario);
+
+        /// <summary>
+        /// Busca consultas de um determinado medico
+        /// </summary>
+        /// <param name="idmedico">Id</param>
+        /// <returns>Retorna Consultas</returns>
+        List<Consultas> BuscarConsultasMedico(int idmedico);
+
+        /// <summary>
+        /// Busca consultas de um determinado prontuario
+        /// </summary>
+        /// <param name="idprontuario">Id</param>
+        /// <returns>Retorna Consultas</returns>
+        List<Consultas> BuscarConsultasProntuario(int idprontuario);
+    }
+}
---
+++ Senai.SPMedicalGroup/Senai.SPMedicalGroup/Repositories/ConsultaRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Senai.SPMedicalGroup.Domains;
+using Senai.SPMedicalGroup.Interfaces;
+
+namespace Senai.SPMedicalGroup.Repositories
+{
+    public class ConsultaRepository : IConsultaRepository
+    {
+        public void Agendar(Consultas consulta)
+        {
+            using (SPMedicalGroupContext ctx = new SPMedicalGroupContext())
+            {
+                ctx.Consultas.Add(consulta);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Atualizar(int id, Consultas consuta)
+        {
+            using (SPMedicalGroupContext ctx = new SPMedicalGroupContext())
+            {
+                Consultas InformacaoConsulta = ctx.Consultas.Find(id);
+
+                InformacaoConsulta.IdMedico = consuta.IdMedico;
+                InformacaoConsulta.IdProntuario = consuta.IdProntuario;
+                InformacaoConsulta.DataHora = consuta.DataHora;
+                InformacaoConsulta.Status = consuta.Status;
+                if (InformacaoConsulta.Descricao != null)
+                {
+                    InformacaoConsulta.Descricao = consuta.Descricao; 
+                }
+
+                ctx.Consultas.Update(consuta);
+                ctx.SaveChanges();
+            }
+        }
+
+        public List<Consultas> Listar()
+        {
+            using (SPMedicalGroupContext ctx = new SPMedicalGroupContext())
+            {
+                return ctx.Consultas.ToList();
+            }
+        }
+    }
+}
